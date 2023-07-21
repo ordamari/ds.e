4addf58aeca59799db5ae2b6be4c5e2cf8744350
ordamari <ordamari971@gmail.com>
2023-07-21 13:03:43 +0300

    fix: fix test again

--- packages/helpers/src/__test__/callFnsInSequence.test.ts
+++ packages/helpers/src/__test__/callFnsInSequence.test.ts
@@ -1,1 +1,20 @@
-it('test');
+import callFnsInSequence from '../callFnsInSequence';
+it('call fns in sequence', () => {
+  const fn1 = jest.fn();
+  const fn2 = jest.fn();
+
+  const fn = callFnsInSequence(fn1, fn2);
+  fn();
+  expect(fn1).toBeCalled();
+  expect(fn2).toBeCalled();
+});
+
+it('call fns in sequence with args', () => {
+  const fn1 = jest.fn();
+  const fn2 = jest.fn();
+
+  const fn = callFnsInSequence(fn1, fn2);
+  fn(1, 2, 3);
+  expect(fn1).toBeCalledWith(1, 2, 3);
+  expect(fn2).toBeCalledWith(1, 2, 3);
+});
